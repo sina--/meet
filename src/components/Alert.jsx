ca29cad6a4fb91f3208fc3d738e2c77b09938910
Sina <sina@thewolpers.com>
2025-03-20 18:18:59 -0700

    Add InfoAlert component

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -30,4 +30,14 @@ class Alert extends Component {
       </div>
     );
   }
-} 
+}
+
+class InfoAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'rgb(0, 0, 255)'; // blue
+    this.bgColor = 'rgb(220, 220, 255)'; // light blue
+  }
+}
+
+export { InfoAlert }; 
