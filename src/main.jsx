b2205f761abc01515f7777ca6fe577f5b0ba5f8a
Sina <sina@thewolpers.com>
2025-03-24 08:39:37 -0700

    Remove reportWebVitals and clean up CRA-specific code

--- src/main.jsx
+++ src/main.jsx
@@ -2,25 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import reportWebVitals from './reportWebVitals';
-import * as atatus from 'atatus-spa';
+// import * as atatus from 'atatus-spa';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-// atatus.config('ad3c2cdec29b481f930ed452ec0fe93a').install();
+// atatus.config('1b1ee5665eb943e3bdb71259beabe47a').install();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
